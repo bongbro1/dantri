f10c5188aabb7320457e1b09053bf07c47e5d954
Bong <bongtham01@gmail.com>
2024-01-11 00:53:19 +0700

    push

---
+++ asset/js/main.js
@@ -0,0 +1,76 @@
+const btn_prev = document.querySelector('.js_btn_prev');
+const btn_next = document.querySelector('.js_btn_next');
+const post_slider = document.querySelector('.js_post_slider'); // frame chứa các post
+const listPost = document.querySelectorAll('.js_post_slider .col-md-3'); // danh sách các post
+var value = 0;
+var maxValue = Math.max(0, (listPost.length - 4)) * (-289.5);
+
+btn_prev.addEventListener('click', () => {
+  if (value < 0) {
+    value += 289.5;
+    post_slider.style.transform = 'translateX(' + value + 'px)';
+    btn_next.style.opacity = '1';
+  }
+  else { // vô hiệu hóa button khi quá cửa số post
+    btn_prev.disable = true;
+    btn_prev.style.opacity = '0.8';
+  }
+})
+btn_next.addEventListener('click', () => {
+  if (value > maxValue) {
+    value -= 289.5;
+    post_slider.style.transform = 'translateX('+ value + 'px)';
+    btn_prev.style.opacity = '1';
+  }
+  else { // vô hiệu hóa button khi quá cửa số post
+    btn_next.disable = true;
+    btn_next.style.opacity = '0.8';
+  }
+})
+
+// btn_prev.addEventListener('click', () => {
+//   const list = document.querySelectorAll('.post_slider .col-md-3');
+//   post_slider.prepend(list[list.length-1]);
+// })
+// btn_next.addEventListener('click', () => {
+//   const list = document.querySelectorAll('.post_slider .col-md-3');
+//   post_slider.appendChild(list[0]);
+// })
+
+// xử lý đổi màu vị trí current page
+const listNumberPage = document.querySelectorAll('.js_post_slider_indexCurrent i');
+var index = 0;
+btn_prev.addEventListener('click', () => {
+  index --;
+  index = Math.max(index, 0);
+  listNumberPage[index+1].classList.remove('active');
+  listNumberPage[index].classList.add('active');
+})
+btn_next.addEventListener('click', () => {
+  index ++;
+  index = Math.min(2, index);
+  listNumberPage[index-1].classList.remove('active');
+  listNumberPage[index].classList.add('active');
+})
+
+// CHỨC NĂNG LÊN ĐẦU TRANG
+const btnUpToTop = document.querySelector('.js_btn_UpToTop');
+document.addEventListener('wheel', (event) => {
+  // console.log(window.scrollY);
+  if (window.scrollY === 0)
+    btnUpToTop.style.display = 'none';
+  else
+    btnUpToTop.style.display = 'block';
+});
+
+btnUpToTop.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+  setTimeout(() => {
+    btnUpToTop.style.display = 'none';
+  }, 200);
+});
+console.log(window.scrollY);
+
